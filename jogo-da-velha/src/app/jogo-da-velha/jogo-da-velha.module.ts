dd57d364e523680bb242edbb82b4c96310cc9440
almir.martinelli <almir@talkcomm.com.br>
2022-10-03 09:21:36 -0300

    seção 5 item 78
    Finalizada

--- jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JogoDaVelhaComponent
+  ],
   imports: [
     CommonModule
+  ],
+  // Faremos o export aqui, pois
+  // não vamos utilizar rotas
+  // para visualizar o html do component
+  exports: [
+    JogoDaVelhaComponent
   ]
 })
 export class JogoDaVelhaModule { }
